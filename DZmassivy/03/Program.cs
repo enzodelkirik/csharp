e170255b3409f4f25dda2d6071492b8d61d63c85
Kirill Linkov <linkovkv1985@gmail.com>
2024-03-26 14:23:45 +0300

    ДЗ по массивам

---
+++ DZmassivy/03/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+*/
+
+
+string inputString = "123454321";
+bool isPalindrome = true;
+
+for (int i = 0; i < inputString.Length / 2; i++)
+{
+    if (inputString[i] != inputString[inputString.Length - 1 - i])
+    {
+        isPalindrome = false;
+        break;
+    }
+}
+
+Console.WriteLine($"Строка {(isPalindrome ? "является" : "не является")} палиндромом");
